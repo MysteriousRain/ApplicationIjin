6f9ab68be692ee818d89e25b8ca5a07858b848a8
satrio keren <satriotrinugroho@gmail.com>
2022-04-20 14:33:42 +0700

    Fitur Added : Image Picker Get Post

--- app/src/main/java/com/example/applicationijin/MainActivity.kt
+++ app/src/main/java/com/example/applicationijin/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.example.applicationijin
 
+import android.app.Activity
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.applicationijin.model.GetIjinResponse
+import com.example.applicationijin.model.ResponsePostData
 import com.example.applicationijin.service.RetrofitConfig
+import com.github.drjacky.imagepicker.ImagePicker
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val uri = it.data?.data!!
+
+                imageView.setImageURI(uri)
+            }
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
 
         RetrofitConfig().getIjin().getAllIjin().enqueue(object :Callback<GetIjinResponse>{
             override fun onResponse(
@@ -29,6 +52,72 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        btnAddImage.setOnClickListener({
+            cameraLauncher.launch(
+                ImagePicker.with(this)
+                    .crop()
+                    .cameraOnly()
+                    .maxResultSize(480, 800, true)
+                    .createIntent()
+            )
+
+        })
+
+        btnSubmit.setOnClickListener({
+            RetrofitConfig().getIjin().addDataForm(spinnerKategori.selectedItem.toString(),txtDariTanggal.text.toString(),
+                txtSampaiTanggal.text.toString(),txtPerihal.text.toString(),txtKeterangan.text.toString() ).enqueue(
+                object:Callback<ResponsePostData>{
+                    override fun onResponse(
+                        call: Call<ResponsePostData>,
+                        response: Response<ResponsePostData>
+                    ) {
+                        Toast.makeText(this@MainActivity,(response.body() as ResponsePostData).message,Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onFailure(call: Call<ResponsePostData>, t: Throwable) {
+                        Log.e("error post data",t.localizedMessage.toString())
+                    }
+
+                }
+            )
+        })
+
+    }
+
+    val calendar = Calendar.getInstance()
+    private fun setupUI(){
+        btnDari.setOnClickListener({
+            DatePickerDialog(this@MainActivity,getCalendarListener(0),calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
+
+        })
+
+        btnSampai.setOnClickListener({
+            DatePickerDialog(this@MainActivity,getCalendarListener(1),calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
+
+        })
+    }
+
+    private fun getCalendarListener(tipe:Int):DatePickerDialog.OnDateSetListener{
+        val calendar = Calendar.getInstance()
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+                calendar.set(Calendar.YEAR,p1)
+                calendar.set(Calendar.MONTH,p2)
+                calendar.set(Calendar.DAY_OF_MONTH,p3)
+
+                val formatDate = "yyyy-MM-dd"
+                val sdf = SimpleDateFormat(formatDate, Locale.US)
+
+                if(tipe==0){
+                    txtDariTanggal.setText(sdf.format(calendar.time))
+                }else{
+                    txtSampaiTanggal.setText(sdf.format(calendar.time))
+                }
+            }
+        }
+        return dateSetListener
 
     }
 }
--- app/src/main/java/com/example/applicationijin/service/IIjin.kt
+++ app/src/main/java/com/example/applicationijin/service/IIjin.kt
@@ -1,9 +1,9 @@
 package com.example.applicationijin.service
 
 import com.example.applicationijin.model.GetIjinResponse
+import com.example.applicationijin.model.ResponsePostData
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.*
 
 
 interface IIjin {
@@ -12,4 +12,10 @@ interface IIjin {
     @Headers("X-Api-Key:7CEA5BD21C9B664D78E37276C91FAB02")
     @GET("api/ijin/all")
     fun getAllIjin(): Call<GetIjinResponse>
+
+    @FormUrlEncoded
+    @Headers("X-Api-Key:7CEA5BD21C9B664D78E37276C91FAB02")
+    @POST("api/ijin/add")
+    fun addDataForm(@Field("kategori") kategori:String, @Field("dari") dari:String,
+                    @Field("sampai") sampai:String, @Field("perihal") perihal:String, @Field("keterangan") keterangan:String):Call<ResponsePostData>
 }
